3cd2175c778dd256b013a5b02ef4031e9be21eb5
Pat Anderson <patrick.b.anderson@gmail.com>
2021-05-17 23:36:46 -0500

    added bar chart for past services

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -63,7 +63,8 @@ import OTCMonth from '../OTCMonth/OTCMonth';
 import OtherSubstancesMonth from '../OtherSubstancesMonth/OtherSubstancesMonth';
 import AllDrugsLastMonth from '../AllDrugsLastMonth/AllDrugsLastMonth';
 
-
+//charts for demographic data
+import PastServicesBar from '../PastServicesBar/PastServicesBar';
 
 
 
@@ -337,6 +338,9 @@ function App() {
 
          <ProtectedRoute exact path="/all_drugs_last_month">
            <AllDrugsLastMonth />
+         </ProtectedRoute>
+         <ProtectedRoute exact path="/past_services_bar">       
+          <PastServicesBar />
          </ProtectedRoute>
 
 
