f24323c31e4789b7cca7a458719b1690b8c23e98
Pat Anderson <patrick.b.anderson@gmail.com>
2021-05-25 15:40:54 -0500

    feature_deleting_header

--- src/components/HallucinogenMonth/HallucinogenMonth.jsx
+++ src/components/HallucinogenMonth/HallucinogenMonth.jsx
@@ -44,17 +44,13 @@ function HallucinogenMonth(){
         hallucinogenDisplay= 
         <Box mx='auto' width="75%" >
           <Card>
-            <CardHeader 
-            title={answer.text}
-            />
-          <CardContent>
-            <Divider />
-            <HallucinogenMonthPie />
-            <p>Hallucinogen in the last month Yes:{hallucinogenMonthlyYes}</p>
-            <p>Hallucinogen in the last month No: {hallucinogenMonthlyNo}</p>
-            <Divider />
-            <p>YourPath assessment takers were given the choice of entering how many days in the previous month they used hallucinogens, including LSD, mushrooms or DMT. This pie graph shows the percentage of people who had used at least one day in the previous month.</p>
-          </CardContent>
+            <CardContent>
+              <HallucinogenMonthPie />
+              <p>Hallucinogen in the last month Yes:{hallucinogenMonthlyYes}</p>
+              <p>Hallucinogen in the last month No: {hallucinogenMonthlyNo}</p>
+              <Divider />
+              <p>YourPath assessment takers were given the choice of entering how many days in the previous month they used hallucinogens, including LSD, mushrooms or DMT. This pie graph shows the percentage of people who had used at least one day in the previous month.</p>
+            </CardContent>
           </Card>
         </Box>
         })
