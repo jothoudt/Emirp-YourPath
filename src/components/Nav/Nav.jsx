ed9ba165fe99587da641d62d421cbdcb6cc754ed
Johnathan <johnathanboyd5@gmail.com>
2021-05-26 11:36:15 -0500

    added transition animation to the dropdown menu

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,9 +6,11 @@ import {useSelector} from 'react-redux';
 import {FiMenu, 
   FiChevronRight,
   FiUser,
-  FiLogOut
+  FiLogOut,
+  FiArrowLeft
 } from "react-icons/fi";
 import {BiBarChartAlt2, BiHeartCircle, BiCapsule, BiInfoCircle} from "react-icons/bi";
+import { CSSTransition } from 'react-transition-group';
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -60,9 +62,17 @@ function NavItem(props) {
 
 function DropdownMenu () {
 
+  const [activeMenu, setActiveMenu] = useState('main'); // settings, about userpage, logout
+  const [menuHeight, setMenuHeight] = useState(null); 
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
   function DropdownItem(props) {
     return(
-      <a className="menu-item">
+      <a className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-left">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
@@ -71,54 +81,92 @@ function DropdownMenu () {
   }
 
   return (
-    <div className="dropdown">
-      <Link to="/user">
-        <DropdownItem
-          leftIcon={<FiUser />}
-          rightIcon={<FiChevronRight />}>
-              <h4 className="link-header">User Page</h4>
-        </DropdownItem>
-      </Link>
+    <div className="dropdown" style={{ height: menuHeight }}>
 
-      <Link to="/drugstatistics">
-        <DropdownItem
-          leftIcon={<BiCapsule />}
-          rightIcon={<FiChevronRight />}>
-            <h4 className="link-header">Drug Usage</h4>
-        </DropdownItem>
-      </Link>
+      <CSSTransition 
+      in={activeMenu === 'main' } 
+      unmountOnExit 
+      timeout={500} 
+      classNames="menu-primary"
+      onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem
+            leftIcon={<FiUser />}
+            >
+              <Link to="/user">
+                {/* <a href="/user" className="user"> */}
+                  <h4 className="link-header">User Page</h4>
+            
+                {/* </a>  */}
+              </Link>
+          </DropdownItem>
 
-      <Link to="/demographics">
-        <DropdownItem
-          leftIcon={<BiBarChartAlt2 />}
-          rightIcon={<FiChevronRight />}>
-            <h4 className="link-header">Demographics</h4>
-        </DropdownItem>
-      
-      </Link>  
+        {/* <Link to="/drugstatistics"> */}
+          <DropdownItem
+            leftIcon={<BiCapsule />}
+            rightIcon={<FiChevronRight />}
+            goToMenu="drug-usage">
+              <h4 className="link-header">Drug Usage</h4>
+          </DropdownItem>
+        {/* </Link> */}
 
-      <Link to="/healthstatistics">
-        <DropdownItem
-          leftIcon={<BiHeartCircle />}
-          rightIcon={<FiChevronRight />}>
-            <h4 className="link-header">Health</h4>
-        </DropdownItem>
-      </Link>  
+        {/* <Link to="/demographics"> */}
+          <DropdownItem
+            leftIcon={<BiBarChartAlt2 />}
+            rightIcon={<FiChevronRight />}>
+              <h4 className="link-header">Demographics</h4>
+          </DropdownItem>
+        
+        {/* </Link>   */}
 
-      <Link to="/about">
-        <DropdownItem
-          leftIcon={<BiInfoCircle />}
-          rightIcon={<FiChevronRight />}>
-            <h4 className="link-header">About</h4>
-           
-        </DropdownItem>
-      </Link>
-      
-        <DropdownItem
-          leftIcon={<FiLogOut />}>
-            <h4 className="link-header">Log Out</h4>
-           
-        </DropdownItem>
+        {/* <Link to="/healthstatistics"> */}
+          <DropdownItem
+            leftIcon={<BiHeartCircle />}
+            rightIcon={<FiChevronRight />}>
+              <h4 className="link-header">Health</h4>
+          </DropdownItem>
+        {/* </Link>   */}
+
+        {/* <Link to="/about"> */}
+          <DropdownItem
+            leftIcon={<BiInfoCircle />}
+            rightIcon={<FiChevronRight />}>
+              <h4 className="link-header">About</h4>
+            
+          </DropdownItem>
+        {/* </Link> */}
+        
+          <DropdownItem
+            leftIcon={<FiLogOut />}>
+              <h4 className="link-header">Log Out</h4>
+            
+          </DropdownItem>
+          </div>
+        </CSSTransition>
+
+        <CSSTransition 
+      in={activeMenu === 'drug-usage' } 
+      unmountOnExit 
+      timeout={500} 
+      classNames="menu-secondary"
+      onEnter={calcHeight}
+      >
+        <div className="menu">
+        <DropdownItem leftIcon={<FiArrowLeft />} goToMenu="main"/>
+          <DropdownItem
+            leftIcon={<FiUser />}
+            >
+              <Link to="/user">
+                {/* <a href="/user" className="user"> */}
+                  <h4 className="link-header">Dashboard</h4>
+            
+                {/* </a>  */}
+              </Link>
+          </DropdownItem>
+
+          </div>
+        </CSSTransition>
     </div>
   )
 }
